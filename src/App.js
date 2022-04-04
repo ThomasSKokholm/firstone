242c98547311c26d54f67458b52082868f98fa8b
ThomasSKokholm <ThomasSKokholm@gmail.com>
2022-04-04 10:34:51 +0200

    Heruko vågn op

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tjek <code>src/App.js</code> og commit ÆØÅ.
         </p>
         <a
           className="App-link"
